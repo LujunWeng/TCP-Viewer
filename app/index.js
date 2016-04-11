01f984233f229b0c323112124f84c1f2eb721dff
Lujun Weng <loyweng@163.com>
2016-04-11 11:54:17 +0800

    can use jquery

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
 'use strict'
 
-window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require('../bower_components/jquery/dist/jquery.js');
+
+$('#hello').html('<em>Hello, World!</em>');
