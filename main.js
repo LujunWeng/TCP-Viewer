c12ea4cae0685ab4c7e05f5543f704ff594fbf13
Lujun Weng <loyweng@163.com>
2016-04-11 10:54:24 +0800

    No menu

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ app.on('window-all-closed', function () {
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
+        autoHideMenuBar: true,
         width: 800, 
         height: 600
     });
