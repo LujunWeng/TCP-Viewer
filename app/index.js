7de10c0700520009da4e1364d8848eae55b358d7
Lujun Weng <loyweng@163.com>
2016-04-15 16:33:15 +0800

    introduce ConnEvent

--- app/index.js
+++ app/index.js
@@ -3,24 +3,39 @@
 window.$ = window.jQuery = require('../bower_components/jquery/dist/jquery.js');
 const childProcess = require('child_process');
 const readLine = require('readline');
-
 const child = childProcess.spawn('../EventTrace/Debug/EventTrace.exe');
 const rl = readLine.createInterface({input: child.stdout});
 
-var Connection = function (conn) {
-    this.pid = conn.PID;
-    this.proto = conn.proto;
-    this.saddr = conn.saddr;
-    this.sport = conn.sport;
-    this.daddr = conn.daddr;
-    this.dport = conn.dport;
-    this.sent = parseInt(conn.size);
-    this.received = parseInt(conn.size);
-    this.state = parseInt(conn.type);
+var Connection = function (ce) {
+    this.pid = ce.pid;
+    this.proto = ce.proto;
+    this.saddr = ce.saddr;
+    this.sport = ce.sport;
+    this.daddr = ce.daddr;
+    this.dport = ce.dport;
+    this.sent = ce.size;
+    this.received = ce.size;
+    this.state = ce.type;
 };
 
-Connection.prototype.generateRow = function() {
+var ConnEvent = function (ce) {
+    this.pid = parseInt(ce.PID);
+    this.proto = ce.proto;
+    this.saddr = ce.saddr;
+    this.sport = ce.sport;
+    this.daddr = ce.daddr;
+    this.dport = ce.dport;
+    this.size = parseInt(ce.size);
+    this.type = parseInt(ce.type);
+};
 
+ConnEvent.prototype.belongsTo = function(conn) {
+    return this.pid === conn.pid &&
+           this.proto === conn.proto &&
+           this.saddr === conn.saddr &&
+           this.sport === conn.sport &&
+           this.daddr === conn.daddr &&
+           this.dport === conn.dport;
 };
 
 var connList = [];
@@ -29,7 +44,7 @@ rl.on('line', (data) => {
     console.log(data);
     let jsonconn = JSON.parse(data);
     if (typeof jsonconn === 'object') {
-        let conn = new Connection(jsonconn);
-        console.log(conn);
+        let ce = new ConnEvent(jsonconn);
+        console.log(ce);
     }
 });
