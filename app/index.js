9db7441e71871acaa5b546f90b093b2458e441f3
Lujun Weng <loyweng@163.com>
2016-04-15 12:43:13 +0800

    create a class to store connection information

--- app/index.js
+++ app/index.js
@@ -6,6 +6,29 @@ const readLine = require('readline');
 
 const child = childProcess.spawn('../EventTrace/Debug/EventTrace.exe');
 const rl = readLine.createInterface({input: child.stdout});
+
+var Connection = function (conn) {
+    this.pid = conn.PID;
+    this.proto = conn.proto;
+    this.saddr = conn.saddr;
+    this.sport = conn.sport;
+    this.daddr = conn.daddr;
+    this.dport = conn.dport;
+    this.sent = parseInt(conn.size);
+    this.received = parseInt(conn.size);
+    this.state = parseInt(conn.type);
+};
+
+Connection.prototype.generateRow = function() {
+
+};
+
+var connList = [];
+
 rl.on('line', (data) => {
-    $('#info').html(['<strong>', data.split(' ').join('-'), '</strong>'].join(''));
+    let jsonconn = JSON.parse(data);
+    if (jsonconn && typeof jsonconn === 'object') {
+        let conn = new Connection(jsonconn);
+        console.log(conn);
+    }
 });
