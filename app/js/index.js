edb0b362bfcda958d78f034643ec0383cc2ab472
Lujun Weng <loyweng@163.com>
2016-04-22 15:55:06 +0800

    state tranformation completed

--- app/js/index.js
+++ app/js/index.js
@@ -11,15 +11,116 @@ var Connection = function (ce) {
     this.sport = ce.sport;
     this.daddr = ce.daddr;
     this.dport = ce.dport;
-    this.sent = ce.size;
-    this.received = ce.size;
-    this.state = ce.type;
+    this.sent = 0;
+    this.received = 0;
+    this.state = 'Unknown';
+    this.refreshState(ce);
+    this.unactiveCnt = 0;
 };
 
-Connection.prototype.refresh = function (ce) {
-    this.sent += ce.size;
-    this.received += ce.size;
-    this.state = ce.type;
+Connection.prototype.refreshState = function (ce) {
+    const tcpStateMap = function (state, type) {
+        if (state === 'Connecting') {
+            switch (type) {
+                case 11:
+                case 10:
+                case 15:
+                case 26:
+                case 27:
+                case 31:
+                    state = 'Established';
+                    break;
+                case 13:
+                case 17:
+                case 29:
+                    state = 'Disconnecting';
+                    break;
+                default :
+                    state = 'Connecting';
+                    break;
+            }
+        } else if (state === 'Disconnecting') {
+            switch (type) {
+                case 12:
+                case 16:
+                case 28:
+                case 29:
+                    state = 'Connecting';
+                    break;
+                case 11:
+                case 10:
+                case 26:
+                case 27:
+                    state = 'Established';
+                    break;
+                default:
+                    state = 'Disconnecting';
+                    break;
+            }
+        } else if (state === 'Established') {
+            switch (type) {
+                case 13:
+                case 29:
+                case 17:
+                    state = 'Disconnecting';
+                    break;
+                default:
+                    state = 'Established';
+                    break;
+            }
+        } else {
+            switch (type) {
+                case 12:
+                case 16:
+                case 28:
+                case 32:
+                    state = 'Connecting';
+                    break;
+                case 13:
+                case 17:
+                case 29:
+                    state = 'Disconnecting';
+                    break;
+                case 11:
+                case 10:
+                case 26:
+                case 27:
+                    state = 'Established';
+                    break;
+                default:
+                    break;
+            }
+        }
+        return state;
+    };
+    const udpStateMap = function (state, type) {
+        switch (type) {
+            case 11:
+            case 27:
+                state = 'Receiving';
+                break;
+            case 10:
+            case 26:
+                state = 'Sending';
+                break;
+            default:
+                state = 'Disconnecting';
+                break;
+        }
+        return state;
+    };
+    const stateMap = {
+        'TCP': tcpStateMap,
+        'UDP': udpStateMap
+    };
+    this.state = stateMap[this.proto](this.state, ce.type);
+    switch (ce.type) {
+        case 10: case 26:
+            this.sent += ce.size;
+            break;
+        case 11: case 27:
+            this.received += ce.size;
+    }
 };
 
 var ConnEvent = function (ce) {
@@ -49,12 +150,29 @@ var ConnList = function () {
 ConnList.prototype.onConnEvent = function (ce) {
     for (let i = 0; i < this.items.length; ++i) {
         if (ce.belongsTo(this.items[i])) {
-            this.items[i].refresh(ce);
+            this.items[i].refreshState(ce);
+            this.items[i].unactiveCnt = 0;
             return;
         }
     }
     let c = new Connection(ce);
-    this.items.push(c);
+    this.items.unshift(c);
+};
+
+ConnList.prototype.removeUnactive = function () {
+    for (let i = 0; i < this.items.length; ++i) {
+        if (this.items[i].state === 'Disconnecting') {
+            this.items.splice(i, 1);
+            continue;
+        }
+        this.items[i].unactiveCnt++;
+        if (this.items[i].proto === 'UDP' && this.items[i].unactiveCnt > 60) {
+            this.items.splice(i, 1);
+
+        } else if (this.items[i].proto === 'TCP' && this.items[i].unactiveCnt > 120) {
+            this.items.splice(i, 1);
+        }
+    }
 };
 
 var ConnTrace = function () {
@@ -69,7 +187,7 @@ ConnTrace.prototype.start = function () {
     this.childProc = childProcess.spawn(this.eventTracePath);
     const rl = readLine.createInterface({input: this.childProc.stdout});
     rl.on('line', (data) => {
-        console.log(data);
+        // console.log(data);
         let jsonconn = JSON.parse(data);
         if (typeof jsonconn === 'object') {
             let ce = new ConnEvent(jsonconn);
@@ -84,9 +202,6 @@ ConnTrace.prototype.stop = function () {
     this.childProc.kill();
     this.childProc = null;
 };
-
-
-
 
 var connTrace = new ConnTrace();
 var refreshing = false;
@@ -134,6 +249,7 @@ function refreshConnListDisplay(cnlist) {
 
 function refresh() {
     refreshConnListDisplay(connTrace.connList.items);
+    connTrace.connList.removeUnactive();
     if (refreshing)
         setTimeout(refresh, 1000);
 }
